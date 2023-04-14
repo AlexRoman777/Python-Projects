2aa74ea873d139d88c0847fe4f25dbf136c90b0c
Alex Roman <112509308+AlexRoman777@users.noreply.github.com>
2023-04-14 19:43:27 +0200

    Update mergepdf.py

--- PDF-Merger/mergepdf.py
+++ PDF-Merger/mergepdf.py
@@ -2,21 +2,26 @@
 from pypdf import PdfWriter
 from tkinter import filedialog
 from tkinter import Tk
+import logging
+from pathlib import Path
 import os
 
 merger = PdfWriter()
 root = Tk()
+
+PDF_FILE_TYPE = ("PDF files", "*.pdf")
+ALL_FILE_TYPE = ("all files", "*.*")
 
 
 def get_input_files():
     '''Returns a list of file paths'''
     root.withdraw()
     root.filename = filedialog.askopenfilenames(
-        initialdir="/", title="Select PDF files for merging", filetypes=(("PDF files", "*.pdf"), ("all files", "*.*")))
+        initialdir="/", title="Select PDF files for merging", filetypes=(PDF_FILE_TYPE, ALL_FILE_TYPE))
     nr_of_files = len(root.filename)
     print(f'{nr_of_files} files selected')
     if nr_of_files == 0:
-        print('No files selected, exiting...')
+        logging.info(f'{nr_of_files} files selected')
         raise SystemExit(1)
     else:
         return root.filename
@@ -26,7 +31,7 @@ def get_output_file():  # TODO: Add a default file name and destination
     '''Returns a file path'''
     root.withdraw()
     root.filename = filedialog.asksaveasfilename(
-        initialdir="/", title="Input file name and choose destination", filetypes=(("PDF files", "*.pdf"), ("all files", "*.*")))
+        initialdir="/", title="Input file name and choose destination", filetypes=(PDF_FILE_TYPE, ALL_FILE_TYPE))
     return root.filename
 
 
@@ -34,6 +39,7 @@ def merge_pdf():
     '''Merges PDF files and saves the output file'''
     files = get_input_files()
     if files is None:
+        logging.info(f'No files selected')
         raise SystemExit(1)
     else:
         for pdf in files:
