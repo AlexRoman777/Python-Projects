250561fe36d9b4c20e75e435f455205629a8f15a
Alex Roman <112509308+AlexRoman777@users.noreply.github.com>
2023-04-15 04:10:49 +0200

    Create users.py

---
+++ DevOps22/data/users.py
@@ -0,0 +1,18 @@
+
+
+user_adresses = ['https://github.com/AlexRoman777', 'https://github.com/AntonOttosson',
+                 'https://github.com/JarlJakobsson', 'https://github.com/mindriddler',
+                 'https://github.com/PapaPeskwo', 'https://github.com/SamanPetfat',
+                 'https://github.com/rlinddo22', 'https://github.com/raffiavakian',
+                 'https://github.com/PavelKostyuk', 'https://github.com/ossonack',
+                 'https://github.com/miralee94', 'https://github.com/MarioKhalaf',
+                 'https://github.com/Manibadani', 'https://github.com/maal2202',
+                 'https://github.com/lundgren98', 'https://github.com/JustOwl',
+                 'https://github.com/julianbe93', 'https://github.com/JonksPanda',
+                 'https://github.com/IsacGrive', 'https://github.com/IsabelValijani',
+                 'https://github.com/gurraoberg', 'https://github.com/fridalundstroms',
+                 'https://github.com/Fradop', 'https://github.com/delu2201',
+                 'https://github.com/davidhasenson', 'https://github.com/DanielBrunoMatzui',
+                 'https://github.com/Corn1344', 'https://github.com/CharalamposMoutsios',
+                 'https://github.com/Skygfisk', 'https://github.com/Sundgren95',
+                 'https://github.com/Telf92', 'https://github.com/Tolli-txt']
